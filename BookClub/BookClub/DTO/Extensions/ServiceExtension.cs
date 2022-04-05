71688a88977ff8084658173bd018fe24b0ec0e90
Moreart1 <morearti1726263@yandex.ru>
2022-04-05 23:30:06 +0300

    Доработка

---
+++ BookClub/BookClub/DTO/Extensions/ServiceExtension.cs
@@ -0,0 +1,13 @@
+﻿using BookClub.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookClub.DTO.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void AddLogicInjection<T>(this IServiceCollection services)
+        {
+            services.AddDbContext<DataBaseContext>(options => options.UseSqlServer("Server=(localdb)\\v12.0;Database=BookClub;Trusted_Connection=True;"), ServiceLifetime.Transient);
+        }
+    }
+}
---
+++ BookClub/BookClub/DTO/Interface/IDataAccessRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookClub.DAL.Interface;
+
+namespace BookClub.DTO.Interface
+{
+    public interface IDataAccessRepository
+    {
+        IUserRepository UserRepository { get; }
+        IBookRepository BookRepository { get; }
+    }
+}
